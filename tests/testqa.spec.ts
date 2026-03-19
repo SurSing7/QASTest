e15ebf74258fef2648cfb3ad2af127651bbe4d35
SurSing7 <sureshid75@gmail.com>
2026-03-19 18:02:35 +0530

    first commit

---
+++ tests/testqa.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('login test', ()=>{
+
+    test('1 login verification', async ({ page }) => {
+        await page.goto('https://www.google.com/');
+        console.log('login');
+        await page.waitForTimeout(6000);
+    });
+
+    test('2 add to cart', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        expect(page).toHaveTitle('Swag Labs');
+        await page.waitForTimeout(4000);
+    });
+
+})
