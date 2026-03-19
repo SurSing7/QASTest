3fb00dc37d0f3ac38968a16adfc78c132c038862
SurSing7 <sureshid75@gmail.com>
2026-03-19 18:32:23 +0530

    fix

--- tests/testqa.spec.ts
+++ tests/testqa.spec.ts
@@ -1,17 +1,15 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test.describe('login test', ()=>{
+test.describe('login test', () => {
+  test('1 login verification', async ({ page }) => {
+    await page.goto('https://www.google.com/');
+    console.log('login');
+    await page.waitForTimeout(6000);
+  });
 
-    test('1 login verification', async ({ page }) => {
-        await page.goto('https://www.google.com/');
-        console.log('login');
-        await page.waitForTimeout(6000);
-    });
-
-    test('2 add to cart', async ({ page }) => {
-        await page.goto('https://www.saucedemo.com/');
-        expect(page).toHaveTitle('Swag Labs');
-        await page.waitForTimeout(4000);
-    });
-
-})
+  test('2 add to cart', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    expect(page).toHaveTitle('Swag Labs');
+    await page.waitForTimeout(4000);
+  });
+});
